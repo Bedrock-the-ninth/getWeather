b6c888c58c12f25fc68207b8000178818bf975a6
Bedrock-the-ninth <ravi.be2002@gmail.com>
2023-08-14 14:59:24 +0330

    Just found out the freaking geolocation api is not available using Iran's IP and therefore all my tries to get the latitude and longitude of the user, successfully failed :), but i think i will develope a freaking input for lat and lng so the user can get dynamic results

--- main.js
+++ main.js
@@ -1,24 +1,26 @@
 const appid = "dac0c43ffcfccadddd05c3ec10c3d838";
 
-const coordsDisplay = () => {
-  const succes = (pos) => {
-    let altAndLon = pos.coords.altitude + "," + pos.coords.longitude;
+const getPosition = (options) => {
+  return new Promise(function (resolve, reject) {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(resolve, reject, options);
+    }
+  });
+};
 
-    $("#res").html = `
-    <div class="card text-center">
-    <h1 class="card-header">You are located at:</h1>
-    <div class="card-body">
-      <p>Your latitude: ${pos.coords.altitude}</p>
-      <p>Your longitude: ${pos.coords.longitude}</p>
-      <p>Accuracy: ${pos.coords.accuracy} meters</p>
-    </div>
-  </div>
-  `;
+const gpsLocation = async () => {
+  try {
+    const geolocationData = await getPosition({
+      enableHighAccuracy: true,
+      // timeout: 5000,
+      // maximumAge: 0,
+    });
 
-    return altAndLon;
-  };
+    const lat = geolocationData.coords.latitude;
+    const lng = geolocationData.coords.longitude;
 
-  const error = (error) => {
+    return [lat, lng];
+  } catch (error) {
     switch (error.code) {
       case error.PERMISSION_DENIED:
         $("#res").html("User denied the request for Geolocation.");
@@ -34,19 +36,32 @@ const coordsDisplay = () => {
         $("#res").html("An unknown error occurred.");
         break;
     }
-  };
-
-  const options = {
-    enableHighAccuracy: true,
-    timeout: 5000,
-    maximumAge: 0,
-  };
-
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(succes, error, options);
-  } else {
-    $("#res").html("Geolocation is not supported");
   }
+};
+
+const timeConvert = (time) => {
+  let data = new Date(time * 1000);
+  let readable = data.toLocaleTimeString("default");
+  return readable;
+};
+
+const showOutput = (res) => {
+  $("#res").html(`
+  <div class="card text-center m-5 p-1">
+    <div class="card-heading d-grid">
+      <h1 class="display-1 d-grid">${res.data.main.temp} degrees
+      <span class="small">feels like ${res.data.main.feels_like}</span></h1>
+      <span class="display-5">The weather: ${
+        res.data.weather[0].description
+      }</span>
+      <span class="display-5">Wind speed: ${res.data.wind.speed}</span>
+    </div>
+    <div class="car-body my-2">
+      <p>You are currently in ${res.data.name}, ${res.data.sys.country}</p>
+      <p>Sunrise in: ${timeConvert(res.data.sys.sunrise)}</p>
+      <p>Sunset in: ${timeConvert(res.data.sys.sunset)}</p>
+    </div>
+  </div>`);
 };
 
 const getWeather = () => {
@@ -55,26 +70,15 @@ const getWeather = () => {
       `https://api.openweathermap.org/data/2.5/weather?lat=36.3109&lon=59.51&appid=${appid}&units=metric`
     )
     .then((res) => {
-      $("#res").html = `
-        <div class="card text-center m-5 p-1">
-          <div class="card-heading display-4 d-grid">
-            <h1>${res.data.main.temp} degrees
-            <span class="small">feels like ${res.data.main.feels_like}</span></h1>
-            <span>Max: ${res.data.main.temp_max}</span>
-            <span>Min: ${res.data.main.temp_min}</span>
-          </div>
-          <div class="car-body">
-            <p>The weather is: ${res.data.weather[0].description}</p>
-            <p>Wind speed: ${res.data.wind.speed}</p>
-            <p>You are currently in ${res.data.sys.country}, ${res.data.name}</p>
-            <p>Sunrise in: ${res.data.sys.sunrise}</p>
-            <p>Sunset in: ${res.data.sys.sunset}</p>
-          </div>
-        </div>`;
+      showOutput(res);
     })
     .catch((err) => console.log(err));
 };
 
-// Event listeners
+// Event listener
 $("#get").click(getWeather);
-$("#getLoc").click(coordsDisplay);
+$("#getLoc").click(async () => {
+  const coordsList = await gpsLocation();
+
+  console.log(coordsList);
+});
